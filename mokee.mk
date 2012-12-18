4de1e62486497267cb4b1f1bbcbbf4fa8bb792bd
martincz <martincz@mokeetek.com>
2012-12-18 13:35:06 +0800

    set vendor/mk path
    Change-Id: Ib46ec50da9553a2abd3137a7301b261a651450bb
    Signed-off-by: martincz <martincz@mokeetek.com>

--- mokee.mk
+++ mokee.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p970/p970.mk)
 
-PRODUCT_NAME := cm_p970
+PRODUCT_NAME := mk_p970
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusBlack
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cm/config/common_versions.mk
+-include vendor/mk/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p970
